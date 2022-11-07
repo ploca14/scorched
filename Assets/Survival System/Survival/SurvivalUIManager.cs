4264a4d45974d4e40211866eab8ec7e27db34105
Niclas Deplazes <niclas.deplazes@bluewin.ch>
2022-11-07 13:10:17 +0100

    adds survival UI elements

---
+++ Assets/Survival System/Survival/SurvivalUIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurvivalUIManager : MonoBehaviour
+{
+    [SerializeField] private SurvivalManager _survivalManager;
+    [SerializeField] private Image _hungerMeter, _thirstMeter, _oxygenMeter, _staminaMeter;
+
+    private void FixedUpdate()
+    {
+        _hungerMeter.fillAmount = _survivalManager.HungerPercent;
+        _thirstMeter.fillAmount = _survivalManager.ThirstPercent;
+        _oxygenMeter.fillAmount = _survivalManager.OxygenPercent;
+        _staminaMeter.fillAmount = _survivalManager.StaminaPercent;
+    }
+}
